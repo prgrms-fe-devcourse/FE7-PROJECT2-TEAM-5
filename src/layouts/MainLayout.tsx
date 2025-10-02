aed0ea640f6fd4a5a51b46ca22c65e31ab970a53
Noh_yg <skyt852@gmail.com>
2025-10-02 11:26:02 +0900

    💄 Sidebar css 추가

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,19 +3,19 @@ import Header from "../components/layout/Header";
 import Sidebar from "../components/layout/Sidebar";
 
 export default function MainLayout() {
-  return (
-    <>
-      <div className="min-h-screen flex flex-col">
-        {/* 헤더 영역 */}
-        <Header />
-        {/* 사이드바 + 메인 */}
-        <div className="flex-1 flex w-[1440px] mx-auto">
-          <Sidebar />
-          <main className="flex-1 overflow-auto">
-            <Outlet />
-          </main>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="min-h-screen flex flex-col">
+				{/* 헤더 영역 */}
+				<Header />
+				{/* 사이드바 + 메인 */}
+				<div className="flex-1 flex w-[1440px] mx-auto">
+					<Sidebar />
+					<main className="flex-1 overflow-auto">
+						<Outlet />
+					</main>
+				</div>
+			</div>
+		</>
+	);
 }
