b6b835d4d0ce2faa7abbb6e307e88d9f1c4ccd40
Noh_yg <skyt852@gmail.com>
2025-10-07 23:11:45 +0900

    💄 로그인, 회원가입 페이지 css 수정

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,7 +4,9 @@ export default function AuthLayout() {
 	return (
 		<>
 			<div className="min-h-screen flex items-center justify-center bg-[#F3F4F6]">
-				<Outlet />
+				<div className="rounded-xl bg-white p-10 w-[400px] shadow-[0_12px_24px_rgba(0,0,0,0.1)]">
+					<Outlet />
+				</div>
 			</div>
 		</>
 	);
