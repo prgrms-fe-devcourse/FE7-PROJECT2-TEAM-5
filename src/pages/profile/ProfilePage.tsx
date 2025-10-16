8e051f81b788f185360ed3841f6ef022bc2b9e30
Noh_yg <skyt852@gmail.com>
2025-10-16 20:06:14 +0900

    ✨ 스켈레톤 ui 코드 추가

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -3,10 +3,12 @@ import DetailCard from "./DetailCard";
 import { useParams } from "react-router";
 import { useProfileStore } from "../../stores/profileStore";
 import { useEffect } from "react";
+import ProfileCadeSkeleton from "../../components/loading/profile/ProfileCadeSkeleton";
+import DetailCardSkeleton from "../../components/loading/profile/DetailCardSkeleton";
 
 export default function ProfilePage() {
 	const { id } = useParams();
-	const { userId, fetchProfile, error } = useProfileStore();
+	const { userId, profile, fetchProfile, loading, error } = useProfileStore();
 
 	const isMyProfile = id === "me" || id === userId;
 	const targetAuthId = !isMyProfile ? id : undefined;
@@ -14,6 +16,25 @@ export default function ProfilePage() {
 	useEffect(() => {
 		fetchProfile(targetAuthId ?? null);
 	}, [fetchProfile, targetAuthId]);
+
+	if (loading)
+		return (
+			<>
+				<div className="mx-auto w-[1024px]">
+					<div className="flex flex-row gap-10 w-full">
+						{/* 왼쪽 프로필 카드 */}
+						<div className="w-[270px]">
+							<ProfileCadeSkeleton profile={profile} />
+						</div>
+
+						{/* 오른쪽 상세 정보 */}
+						<div className="flex-1 space-y-[21px]">
+							<DetailCardSkeleton />
+						</div>
+					</div>
+				</div>
+			</>
+		);
 
 	if (error) return <p>❌ 오류: {error}</p>;
 
