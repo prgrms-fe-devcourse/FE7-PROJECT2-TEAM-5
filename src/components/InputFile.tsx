c9e9c3485e7945abf8d55bc8210558d2ef6c1c89
“moming109” <skhu202013029@gmail.com>
2025-10-17 12:16:36 +0900

    🎨 파일 input 컴포넌트 분리 후 적용

---
+++ src/components/InputFile.tsx
@@ -0,0 +1,93 @@
+import { X } from "lucide-react";
+
+type InputFileProps = {
+	imgFiles: { file: string; fileName: string }[];
+	setImgFiles: React.Dispatch<
+		React.SetStateAction<
+			{
+				file: string;
+				fileName: string;
+			}[]
+		>
+	>;
+	isMulti: boolean;
+};
+
+export default function InputFile(props: InputFileProps) {
+	const handleImgFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files = e.target.files;
+		if (files) {
+			Array.from(files).map((file) => {
+				const reader = new FileReader();
+				reader.onload = (e) => {
+					props.setImgFiles((prev) => [
+						...prev,
+						{
+							file: e.target?.result as string,
+							fileName: file.name,
+						},
+					]);
+				};
+				reader.readAsDataURL(file);
+			});
+		}
+	};
+	const removeImgFiles = () => {
+		props.setImgFiles([]);
+	};
+	return (
+		<>
+			{props.imgFiles[0] && (
+				<div className="relative flex flex-col items-center px-6 py-4 rounded-xl bg-white border-1 border-[#E5E7EB]">
+					<div className="relative pb-2">
+						<img
+							src={props.imgFiles[0].file}
+							alt={"image" + 0}
+							className="relative z-2 max-h-50 min-h-30 object-cover bg-white"
+						/>
+						{props.imgFiles.length > 1 && (
+							<div>
+								<img
+									src={props.imgFiles[0].file}
+									alt={"image" + 0}
+									className="absolute top-2 left-2 z-1 max-h-50 min-h-30 object-cover opacity-50"
+								/>
+								<div className="absolute z-2 -bottom-2 -right-4 px-3.5 py-1.5 text-xm text-[#6B7280] font-bold bg-white border-1 border-[#E5E7EB] rounded-3xl">
+									{props.imgFiles.length}
+								</div>
+							</div>
+						)}
+					</div>
+
+					<button
+						type="button"
+						className="absolute top-1.5 right-1.5 p-1 rounded-xl text-red-500 border-1 border-[#E5E7EB] cursor-pointer"
+						onClick={removeImgFiles}
+					>
+						<X size={18} />
+					</button>
+				</div>
+			)}
+			{!props.imgFiles[0] && (
+				<div className="flex flex-col items-center px-6 py-4 rounded-xl bg-white border-2 border-[#E5E7EB] border-dashed">
+					<input
+						id="imgFile"
+						accept="image/*"
+						className="hidden"
+						type="file"
+						name="imgFile"
+						onChange={handleImgFileUpload}
+						multiple={props.isMulti}
+					/>
+					<p className="text-[#6B7280]">Upload image</p>
+					<label
+						htmlFor="imgFile"
+						className="px-6 py-4 rounded-xl text-[#6B7280] bg-[#E5E7EB] cursor-pointer"
+					>
+						Choose Img File
+					</label>
+				</div>
+			)}
+		</>
+	);
+}
--- src/pages/post/PostCreatePage.tsx
+++ src/pages/post/PostCreatePage.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { X } from "lucide-react";
 import { Link, useNavigate } from "react-router";
 import supabase from "../../utils/supabase";
 import { useProfileStore } from "../../stores/profileStore";
 import Input from "../../components/Input";
+import InputFile from "../../components/InputFile";
 
 // 게시글 생성 페이지
 export default function PostCreatePage() {
@@ -76,28 +76,6 @@ export default function PostCreatePage() {
 		}
 	};
 
-	const handleImgFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const files = e.target.files;
-		if (files) {
-			Array.from(files).map((file) => {
-				const reader = new FileReader();
-				reader.onload = (e) => {
-					setImgFiles((prev) => [
-						...prev,
-						{
-							file: e.target?.result as string,
-							fileName: file.name,
-						},
-					]);
-				};
-				reader.readAsDataURL(file);
-			});
-		}
-	};
-
-	const removeImgFiles = () => {
-		setImgFiles([]);
-	};
 	const removeTag = (index: number) => {
 		const newHashTag = [
 			...hashTag.slice(0, index),
@@ -170,57 +148,11 @@ export default function PostCreatePage() {
 							</Input>
 						</div>
 
-						{imgFiles[0] && (
-							<div className="relative flex flex-col items-center px-6 py-4 rounded-xl bg-white border-1 border-[#E5E7EB]">
-								<div className="relative pb-2">
-									<img
-										src={imgFiles[0].file}
-										alt={"image" + 0}
-										className="relative z-2 max-h-50 min-h-30 object-cover bg-white"
-									/>
-									{imgFiles.length > 1 && (
-										<div>
-											<img
-												src={imgFiles[0].file}
-												alt={"image" + 0}
-												className="absolute top-2 left-2 z-1 max-h-50 min-h-30 object-cover opacity-50"
-											/>
-											<div className="absolute z-2 -bottom-2 -right-4 px-3.5 py-1.5 text-xm text-[#6B7280] font-bold bg-white border-1 border-[#E5E7EB] rounded-3xl">
-												{imgFiles.length}
-											</div>
-										</div>
-									)}
-								</div>
-
-								<button
-									type="button"
-									className="absolute top-1.5 right-1.5 p-1 rounded-xl text-red-500 border-1 border-[#E5E7EB] cursor-pointer"
-									onClick={removeImgFiles}
-								>
-									<X size={18} />
-								</button>
-							</div>
-						)}
-						{!imgFiles[0] && (
-							<div className="flex flex-col items-center px-6 py-4 rounded-xl bg-white border-2 border-[#E5E7EB] border-dashed">
-								<input
-									id="imgFile"
-									accept="image/*"
-									className="hidden"
-									type="file"
-									name="imgFile"
-									onChange={handleImgFileUpload}
-									multiple
-								/>
-								<p className="text-[#6B7280]">Upload image</p>
-								<label
-									htmlFor="imgFile"
-									className="px-6 py-4 rounded-xl text-[#6B7280] bg-[#E5E7EB] cursor-pointer"
-								>
-									Choose Img File
-								</label>
-							</div>
-						)}
+						<InputFile
+							imgFiles={imgFiles}
+							setImgFiles={setImgFiles}
+							isMulti={true}
+						/>
 						<div className="relative w-full px-6 py-4 rounded-xl bg-white border-1 border-[#E5E7EB] outline-none user-invalid:border-red-500">
 							<textarea
 								id="content"
