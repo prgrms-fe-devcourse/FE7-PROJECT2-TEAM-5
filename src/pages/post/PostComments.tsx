aa3794388ee6e12dd450ab65c7afbe715e7d34e7
“moming109” <skhu202013029@gmail.com>
2025-10-20 17:00:21 +0900

    ✨PostDetailPage에 필요한 데이터 불러오기2

---
+++ src/pages/post/PostComments.tsx
@@ -0,0 +1,237 @@
+import { Heart, MessageSquare } from "lucide-react";
+import type { Database } from "../../types/database";
+import { useState } from "react";
+import supabase from "../../utils/supabase";
+import { useNavigate } from "react-router";
+import { useProfileStore } from "../../stores/profileStore";
+import { getAge } from "../../utils/getAge";
+import { getGrade } from "../../utils/getGrade";
+
+type Comment = Database["public"]["Tables"]["comments"]["Row"] & {
+	user: {
+		nickname?: string;
+		birth_date: Date;
+		representative_badge_id: {
+			badges: {
+				name: string;
+				icon_url: string;
+			};
+		};
+	};
+	comment_likes?: string[] | null;
+};
+type PostCommentsProps = {
+	comments: Comment[] | null | undefined;
+	postId: string | undefined;
+};
+
+export default function PostComments(props: PostCommentsProps) {
+	const navigate = useNavigate();
+	const [inputComment, setInputComment] = useState("");
+	const [mention, setMention] = useState("");
+	const user = useProfileStore((state) => state.currentUserId);
+
+	const writeComment = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (inputComment.trim() === "") return;
+		try {
+			console.log(props.postId, user);
+			const { data: commentData, error } = await supabase
+				.from("comments")
+				.insert([
+					{
+						post_id: props.postId,
+						user_id: user,
+						content: inputComment,
+					},
+				])
+				.select();
+
+			if (error) throw error;
+			if (commentData) {
+				alert("댓글이 등록되었습니다.");
+				setInputComment("");
+				navigate("/posts/" + props.postId);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	const replyCounts: Record<string, string[]> = {};
+	props.comments?.forEach((c) => {
+		if (c.parent_comment_id) {
+			if (!replyCounts[c.parent_comment_id]) {
+				replyCounts[c.parent_comment_id] = [];
+			}
+			replyCounts[c.parent_comment_id].push(c.id);
+		}
+	});
+
+	// const commentData = props.comments?.map((comment) => ({
+	// 	...comment,
+	// 	reply_count: replyCounts[comment.id] ? replyCounts[comment.id] : 0,
+	// }));
+
+	function Comment({ comment }: { comment: Comment }) {
+		return (
+			<div className="w-full px-4 py-3 rounded-xl bg-white shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
+				{/* 글 제목과 좋아요, 댓글 수 */}
+				<div className="flex justify-between items-start mb-1">
+					<div className="flex gap-1 items-center">
+						<div className="w-[35px] h-[35px]">
+							<img src="/src/assets/image.png" alt="userImg" />
+						</div>
+						<div>
+							{comment.user.representative_badge_id && (
+								<p className="text-xs font-medium">
+									<img
+										src={
+											comment.user.representative_badge_id
+												.badges.icon_url
+										}
+									/>
+									{comment.user.representative_badge_id.badges
+										.name ?? ""}
+								</p>
+							)}
+							<p className="text-sm">
+								{comment.user.nickname}
+								<span className="text-xs text-[#6B7280] ml-1">
+									{getGrade(getAge(comment.user.birth_date))}
+								</span>
+							</p>
+						</div>
+					</div>
+
+					{/* 좋아요, 댓글 수 */}
+					<div className="flex gap-3">
+						{/* 좋아요 개수 표시*/}
+						<button
+							type="button"
+							className="flex gap-1 items-top cursor-pointer"
+						>
+							<Heart color="red" size={15} className="mt-0.5" />
+							<p className="text-[14px]">
+								{comment.comment_likes?.length}
+							</p>
+						</button>
+						{/* 댓글 개수 표시 */}
+						{!comment.parent_comment_id && (
+							<button
+								type="button"
+								className="flex gap-1 items-top cursor-pointer"
+							>
+								<MessageSquare
+									color="#8B5CF6"
+									size={15}
+									className="mt-0.5"
+								/>
+								<p className="text-[14px]">
+									{replyCounts[comment.id]?.length ?? 0}
+								</p>
+							</button>
+						)}
+						{comment.parent_comment_id && (
+							<button
+								type="button"
+								className="flex gap-1 items-top"
+							>
+								<MessageSquare
+									color="#8B5CF6"
+									size={15}
+									className="mt-0.5"
+								/>
+								<p className="text-[14px]">
+									{replyCounts[comment.id].length}
+								</p>
+							</button>
+						)}
+					</div>
+				</div>
+				{/* 댓글 내용 */}
+				<p className="mt-3 mb-2 text-sm font-Regular text-[#6B7280]">
+					{comment.content}
+				</p>
+				{/* 답글 버튼, 작성일 */}
+				{!comment.parent_comment_id && (
+					<div className="flex justify-between">
+						<button
+							type="button"
+							className="text-xs text-[#6B7280] cursor-pointer"
+						>
+							답글달기
+						</button>
+						<p className="text-xs text-[#6B7280]">
+							{comment.created_at.slice(0, 10)}
+						</p>
+					</div>
+				)}
+				{comment.parent_comment_id && (
+					<div className="flex justify-end">
+						<p className="text-xs text-[#6B7280]">
+							{comment.created_at}
+						</p>
+					</div>
+				)}
+			</div>
+		);
+	}
+
+	function CommentItem({ comments }: { comments: Comment[] }) {
+		return (
+			<>
+				{comments.map((comment) => (
+					<div key={comment.id} className="flex flex-col gap-y-3">
+						{/* 원 댓글 */}
+						<Comment comment={comment} />
+
+						{/* 대댓글 */}
+						{replyCounts[comment.id]?.map((replyId) => {
+							const replyComment = comments.find(
+								(c) => c.id === replyId,
+							);
+							return replyComment ? (
+								<Comment
+									key={replyComment.id}
+									comment={replyComment}
+								/>
+							) : null;
+						})}
+					</div>
+				))}
+			</>
+		);
+	}
+
+	return (
+		<>
+			{!props.comments && (
+				<div className="text-center text-gray-500 py-12">
+					현재 게시물에 등록된 댓글이 없습니다.
+				</div>
+			)}
+			{props.comments && (
+				<div className="flex flex-col gap-y-3 h-80 pr-2 overflow-y-auto ">
+					{/* 댓글 1 */}
+					<CommentItem comments={props.comments} />
+				</div>
+			)}
+			<form className="flex gap-2 mt-4 w-full " onSubmit={writeComment}>
+				<input
+					type="textarea"
+					placeholder="댓글을 작성해주세요."
+					value={inputComment}
+					onChange={(e) => setInputComment(e.target.value)}
+					className="w-[696px] text-sm px-6 py-3 border-1 border-[#E5E7EB] rounded-xl focus:outline-none bg-white"
+				/>
+				<button
+					type="submit"
+					className="px-4 py-2.5 text-sm text-white rounded-xl bg-[#8B5CF6] cursor-pointer"
+				>
+					등록
+				</button>
+			</form>
+		</>
+	);
+}
--- src/pages/post/PostDetailPage.tsx
+++ src/pages/post/PostDetailPage.tsx
@@ -1,11 +1,21 @@
 import { Heart, MessageSquare } from "lucide-react";
-import { useNavigate, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import type { Database } from "../../types/database";
 import { useEffect, useState } from "react";
 import supabase from "../../utils/supabase";
+import PostComments from "./PostComments";
 
-type comment = Database["public"]["Tables"]["comments"]["Row"] & {
-	user: { nickname?: string; birth_date: string };
+type Comment = Database["public"]["Tables"]["comments"]["Row"] & {
+	user: {
+		nickname?: string;
+		birth_date: Date;
+		representative_badge_id: {
+			badges: {
+				name: string;
+				icon_url: string;
+			};
+		};
+	};
 	comment_likes?: string[] | null;
 };
 
@@ -13,9 +23,14 @@ type DetailPost = Database["public"]["Tables"]["posts"]["Row"] & {
 	user?: {
 		nickname: string;
 	} | null;
-	files?: string[] | null;
+	files?:
+		| {
+				file_path: string;
+				file_name: string;
+		  }[]
+		| undefined;
+
 	likes?: number | null;
-	comments?: comment[];
 };
 
 // 게시글 세부 페이지
@@ -23,7 +38,8 @@ export default function PostDetailPage() {
 	const navigate = useNavigate();
 	const { id } = useParams();
 	const [isLoading, setIsLoading] = useState(true);
-	const [post, setPost] = useState<DetailPost | null>(null);
+	const [postData, setPost] = useState<DetailPost | null>(null);
+	const [comments, setComments] = useState<Comment[]>([]);
 
 	useEffect(() => {
 		const fetchPost = async () => {
@@ -31,7 +47,9 @@ export default function PostDetailPage() {
 				//게시물 정보 (작성자 닉네임, 첨부 파일도)
 				const { data: post, error: postError } = await supabase
 					.from("posts")
-					.select("*, user:users(nickname), files(file_path)")
+					.select(
+						"*, user:users(nickname, representative_badge_id(badges(name,icon_url))), files(file_path, file_name)",
+					)
 					.eq("id", id)
 					.single();
 				if (postError) throw postError;
@@ -47,12 +65,13 @@ export default function PostDetailPage() {
 				const { data: comments, error: commentsError } = await supabase
 					.from("comments")
 					.select(
-						"*, user:users(nickname, birth_date), comment_likes(id)",
+						"*, user:users(nickname, birth_date, representative_badge_id(badges(name,icon_url))), comment_likes(id)",
 					)
 					.eq("post_id", id);
 				if (commentsError) throw commentsError;
 
-				setPost({ ...post, ...likes, ...comments });
+				setPost({ ...post, ...likes });
+				setComments(comments);
 				setIsLoading(false);
 			} catch (e) {
 				console.error(e);
@@ -68,7 +87,7 @@ export default function PostDetailPage() {
 					<div className="flex justify-between items-center">
 						{/* 글 제목 */}
 						<h1 className="font-bold text-[32px] text-[#8B5CF6]">
-							{post?.title}
+							{postData?.title}
 						</h1>
 						{/* 작성자만 보이는 수정, 삭제 버튼 */}
 						<div className="flex gap-2">
@@ -88,287 +107,42 @@ export default function PostDetailPage() {
 					</div>
 					{/* 작성자, 작성일 */}
 					<p className="text-sm text-[#6B7280]">
-						작성자: {post?.user?.nickname} |{" "}
-						{p ost?.created_at.slice(0, 10)}
+						작성자: {postData?.user?.nickname} |{" "}
+						{postData?.created_at.slice(0, 10)}
 					</p>
 					{/* 본문 */}
 					<div className="pt-9 pb-15">
-						<pre>{post?.content}</pre>
+						<pre>{postData?.content}</pre>
+						<div className="mt-2">
+							{postData?.files?.map((file) => (
+								<img
+									key={file.file_path}
+									src={file.file_path}
+									alt={file.file_name}
+									className="max-h-80"
+								/>
+							))}
+						</div>
+					</div>
+					<div className="flex gap-2 mb-4">
+						{postData?.hash_tag?.map((tag, idx) => (
+							<div
+								key={idx}
+								className="px-2 py-1 rounded-lg bg-[#EDE9FE] font-Regular text-xs text-[#8B5CF6]"
+							>
+								#{tag}
+							</div>
+						))}
 					</div>
 					{/* 댓글 영역 */}
+					<PostComments comments={comments} postId={id} />
 
-					<div className="flex flex-col gap-y-3 h-80 pr-2 overflow-y-auto ">
-						{/* 댓글 1 */}
-						<div className="flex flex-col gap-y-3">
-							{/* 원 댓글 1 */}
-							<div className="w-full px-4 py-3 rounded-xl bg-white shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
-								{/* 글 제목과 좋아요, 댓글 수 */}
-								<div className="flex justify-between items-start mb-1">
-									<div className="flex gap-1 items-center">
-										<div className="w-[35px] h-[35px]">
-											<img
-												src="/src/assets/image.png"
-												alt="userImg"
-											/>
-										</div>
-										<div>
-											<p className="text-xs font-medium">
-												🏆 초보 수학 마스터
-											</p>
-											<p className="text-sm">
-												홍길동
-												<span className="text-xs text-[#6B7280] ml-1">
-													중학교 1학년
-												</span>
-											</p>
-										</div>
-									</div>
-
-									{/* 좋아요, 댓글 수 */}
-									<div className="flex gap-3">
-										{/* 좋아요 개수 표시*/}
-										<div className="flex gap-1 items-top">
-											<Heart
-												color="red"
-												size={15}
-												className="mt-0.5"
-											/>
-											<p className="text-[14px]">2</p>
-										</div>
-										{/* 댓글 개수 표시 */}
-										<div className="flex gap-1 items-top">
-											<MessageSquare
-												color="#8B5CF6"
-												size={15}
-												className="mt-0.5"
-											/>
-											<p className="text-[14px]">2</p>
-										</div>
-									</div>
-								</div>
-								{/* 댓글 내용 */}
-								<p className="mt-3 mb-2 text-sm font-Regular text-[#6B7280]">
-									cos(A+B) = cosAcosB - sinAsinB 입니다!
-								</p>
-								{/* 답글 버튼, 작성일 */}
-								<div className="flex justify-between">
-									<button
-										type="button"
-										className="text-xs text-[#6B7280] cursor-pointer"
-									>
-										답글달기
-									</button>
-									<p className="text-xs text-[#6B7280]">
-										2025-10-10
-									</p>
-								</div>
-							</div>
-							{/* 대댓글은 아래로 추가 */}
-							<div className="w-full px-4 py-3 rounded-xl bg-white shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
-								{/* 글 제목과 좋아요, 댓글 수 */}
-								<div className="flex justify-between items-start mb-1">
-									<div className="flex gap-1 items-center">
-										<div className="w-[35px] h-[35px]">
-											<img
-												src="/src/assets/image.png"
-												alt="userImg"
-											/>
-										</div>
-										<div>
-											<p className="text-xs font-medium">
-												🏆 초보 수학 마스터
-											</p>
-											<p className="text-sm">
-												홍길동
-												<span className="text-xs text-[#6B7280] ml-1">
-													중학교 1학년
-												</span>
-											</p>
-										</div>
-									</div>
-
-									{/* 좋아요, 댓글 수 */}
-									<div className="flex gap-3">
-										{/* 좋아요 개수 표시*/}
-										<div className="flex gap-1 items-top">
-											<Heart
-												color="red"
-												size={15}
-												className="mt-0.5"
-											/>
-											<p className="text-[14px]">2</p>
-										</div>
-										{/* 대댓글이므로 댓글 개수 표시 X
-									<div className="flex gap-1 items-top">
-										<img
-											className="object-none"
-											src="/src/assets/SpeechBubble.png"
-											alt="SpeechBubble"
-										/>
-										<p className="text-[14px]">2</p>
-									</div> */}
-									</div>
-								</div>
-								{/* 댓글 내용, 작성일 */}
-								<div className="flex justify-between items-end">
-									<p className="mt-3 text-sm font-Regular text-[#6B7280]">
-										<span className="mr-1 text-[#8B5CF6] ">
-											@홍길동
-										</span>
-										cos(A+B) = cosAcosB - sinAsinB 입니다!
-									</p>
-									<p className="text-xs text-[#6B7280]">
-										2025-10-10
-									</p>
-								</div>
-							</div>
-						</div>
-						{/* 댓글 1 */}
-						<div className="flex flex-col gap-y-3">
-							{/* 원 댓글 1 */}
-							<div className="w-full px-4 py-3 rounded-xl bg-white shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
-								{/* 글 제목과 좋아요, 댓글 수 */}
-								<div className="flex justify-between items-start mb-1">
-									<div className="flex gap-1 items-center">
-										<div className="w-[35px] h-[35px]">
-											<img
-												src="/src/assets/image.png"
-												alt="userImg"
-											/>
-										</div>
-										<div>
-											<p className="text-xs font-medium">
-												🏆 초보 수학 마스터
-											</p>
-											<p className="text-sm">
-												홍길동
-												<span className="text-xs text-[#6B7280] ml-1">
-													중학교 1학년
-												</span>
-											</p>
-										</div>
-									</div>
-
-									{/* 좋아요, 댓글 수 */}
-									<div className="flex gap-3">
-										{/* 좋아요 개수 표시*/}
-										<div className="flex gap-1 items-top">
-											<Heart
-												color="red"
-												size={15}
-												className="mt-0.5"
-											/>
-											<p className="text-[14px]">2</p>
-										</div>
-										{/* 댓글 개수 표시 */}
-										<div className="flex gap-1 items-top">
-											<MessageSquare
-												color="#8B5CF6"
-												size={15}
-												className="mt-0.5"
-											/>
-											<p className="text-[14px]">2</p>
-										</div>
-									</div>
-								</div>
-								{/* 댓글 내용 */}
-								<p className="mt-3 mb-2 text-sm font-Regular text-[#6B7280]">
-									cos(A+B) = cosAcosB - sinAsinB 입니다!
-								</p>
-								{/* 답글 버튼, 작성일 */}
-								<div className="flex justify-between">
-									<button
-										type="button"
-										className="text-xs text-[#6B7280] cursor-pointer"
-									>
-										답글달기
-									</button>
-									<p className="text-xs text-[#6B7280]">
-										2025-10-10
-									</p>
-								</div>
-							</div>
-							{/* 대댓글은 아래로 추가 */}
-							<div className="w-full px-4 py-3 rounded-xl bg-white shadow-[0_4px_20px_rgba(0,0,0,0.05)]">
-								{/* 글 제목과 좋아요, 댓글 수 */}
-								<div className="flex justify-between items-start mb-1">
-									<div className="flex gap-1 items-center">
-										<div className="w-[35px] h-[35px]">
-											<img
-												src="/src/assets/image.png"
-												alt="userImg"
-											/>
-										</div>
-										<div>
-											<p className="text-xs font-medium">
-												🏆 초보 수학 마스터
-											</p>
-											<p className="text-sm">
-												홍길동
-												<span className="text-xs text-[#6B7280] ml-1">
-													중학교 1학년
-												</span>
-											</p>
-										</div>
-									</div>
-
-									{/* 좋아요, 댓글 수 */}
-									<div className="flex gap-3">
-										{/* 좋아요 개수 표시*/}
-										<div className="flex gap-1 items-top">
-											<Heart
-												color="red"
-												size={15}
-												className="mt-0.5"
-											/>
-											<p className="text-[14px]">2</p>
-										</div>
-										{/* 대댓글이므로 댓글 개수 표시 X
-									<div className="flex gap-1 items-center">
-										<img
-											className="object-none"
-											src="/src/assets/SpeechBubble.png"
-											alt="SpeechBubble"
-										/>
-										<p className="text-[14px]">2</p>
-									</div> */}
-									</div>
-								</div>
-								{/* 댓글 내용, 작성일 */}
-								<div className="flex justify-between items-center">
-									<p className="mt-3 text-sm font-Regular text-[#6B7280]">
-										<span className="mr-1 text-[#8B5CF6] ">
-											@홍길동
-										</span>
-										cos(A+B) = cosAcosB - sinAsinB 입니다!
-									</p>
-									<p className="text-xs text-[#6B7280]">
-										2025-10-10
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<form className="flex gap-2 mt-4 w-full ">
-						<input
-							type="textarea"
-							placeholder="댓글을 작성해주세요."
-							className="w-[696px] text-sm px-6 py-3 border-1 border-[#E5E7EB] rounded-xl focus:outline-none bg-white"
-						/>
-						<button
-							type="button"
-							className="px-4 py-2.5 text-sm text-white rounded-xl bg-[#8B5CF6] cursor-pointer"
-						>
-							등록
-						</button>
-					</form>
-					<button
-						type="button"
-						className="mt-7 px-6 py-2.5 text-sm text-white rounded-xl bg-[#8B5CF6] cursor-pointer"
+					<Link
+						to={"/posts"}
+						className="inline-block mt-7 px-6 py-2.5 text-sm text-white rounded-xl bg-[#8B5CF6] cursor-pointer"
 					>
 						← 게시판으로 돌아가기
-					</button>
+					</Link>
 				</div>
 			</>
 		);
