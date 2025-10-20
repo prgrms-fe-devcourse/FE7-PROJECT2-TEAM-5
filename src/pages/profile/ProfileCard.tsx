20a76578eb53582eb901a8e32fee20636fca3dfc
Noh_yg <skyt852@gmail.com>
2025-10-20 12:54:33 +0900

    ✨ 이미지 등록 기능 추가

--- src/pages/profile/ProfileCard.tsx
+++ src/pages/profile/ProfileCard.tsx
@@ -8,6 +8,7 @@ import { SquarePen, Trash2 } from "lucide-react";
 import Modal from "../../components/Modal";
 import Button from "../../components/Button";
 import type { UserProfile } from "../../types/profile";
+import supabase from "../../utils/supabase";
 
 type Props = {
 	profile: UserProfile | null;
@@ -16,8 +17,12 @@ type Props = {
 export default function ProfileCard({ profile }: Props) {
 	const navigate = useNavigate();
 	const { deleteProfile, currentUserId } = useProfileStore();
-	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+	const [isImgModalOpen, setIsImgModalOpen] = useState(false);
 	const [isDeleting, setIsDeleting] = useState(false);
+	const [isUpdating, setIsUpdating] = useState(false);
+
+	const [imgFile, setImgFile] = useState<string | null>(null);
 
 	if (!profile) return null;
 
@@ -40,7 +45,7 @@ export default function ProfileCard({ profile }: Props) {
 		setIsDeleting(true);
 		try {
 			await deleteProfile();
-			setIsModalOpen(false);
+			setIsDeleteModalOpen(false);
 			navigate("/");
 		} catch (err) {
 			console.error("탈퇴 실패:", err);
@@ -48,30 +53,115 @@ export default function ProfileCard({ profile }: Props) {
 			setIsDeleting(false);
 		}
 	};
+
+	const handleImgFileUpload = async (
+		e: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		const file = e.target.files && e.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = (e) => {
+				setImgFile(e.target?.result as string);
+			};
+			reader.readAsDataURL(file);
+		}
+	};
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setIsUpdating(true);
+
+		if (!imgFile) {
+			console.error("이미지 URL이 비어 있습니다.");
+			return;
+		}
+
+		try {
+			const { error } = await supabase
+				.from("users")
+				.update({ profile_image_url: imgFile })
+				.eq("auth_id", profile.auth_id);
+
+			if (error) throw error;
+
+			console.log("프로필 이미지 URL 업데이트 완료:", imgFile);
+			alert("프로필 이미지가 업데이트되었습니다!");
+			setIsImgModalOpen(false);
+		} catch (err) {
+			console.error("이미지 URL 등록 실패:", err);
+			alert("이미지 URL 저장 중 오류가 발생했습니다.");
+		} finally {
+			setIsUpdating(false);
+
+			// 페이지 새로고침
+			window.location.reload();
+		}
+	};
+
 	return (
 		<>
 			{/* // 왼쪽 영역 - 프로필 카드 */}
 			<div className="flex flex-col items-center relative">
 				{/* 프로필 이미지 영역 */}
 				<div className="z-10 w-30 h-30">
-					<div className="relative w-full h-full rounded-xl cursor-pointer">
+					<div className="relative w-full h-full rounded-xl">
 						<img
 							className="img w-full h-full rounded-xl object-cover"
 							src={profile.profile_image_url ?? basicImage}
 							alt="profile"
 						/>
 						{isMyProfile && (
-							<div className="absolute top-0 left-0 w-full h-full rounded-xl bg-[rgba(0,0,0,0.4)] bg-opacity-40 opacity-0 hover:opacity-100 flex items-center justify-center text-white font-medium transition-opacity">
-								<label htmlFor="profile_img">
+							<>
+								<Button
+									onClick={() => setIsImgModalOpen(true)}
+									className="cursor-pointer absolute top-0 left-0 w-full h-full rounded-xl bg-[rgba(0,0,0,0.4)] bg-opacity-40 opacity-0 hover:opacity-100 flex items-center justify-center text-white font-medium transition-opacity"
+								>
 									<SquarePen size={24} />
-								</label>
-								<input
-									id="profile_img"
-									type="file"
-									accept="image/*"
-									className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-								/>
-							</div>
+								</Button>
+								<Modal
+									isOpen={isImgModalOpen}
+									onClose={() => setIsImgModalOpen(false)}
+									title="프로필 수정"
+								>
+									<form method="post" onSubmit={handleSubmit}>
+										<input
+											id="profile_img"
+											type="file"
+											accept="image/*"
+											name="profileImg"
+											onChange={handleImgFileUpload}
+											className="border"
+										/>
+										<div className="flex justify-center gap-3">
+											{isUpdating ? (
+												""
+											) : (
+												<>
+													<Button
+														onClick={() =>
+															setIsImgModalOpen(
+																false,
+															)
+														}
+														className="px-8 py-3 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300 transition-colors"
+													>
+														취소
+													</Button>
+												</>
+											)}
+
+											<Button
+												disabled={isUpdating}
+												className="px-8 py-3 rounded-md bg-violet-500 text-white hover:bg-violet-600 transition-colors"
+											>
+												{isUpdating
+													? "수정 중..."
+													: "수정"}
+											</Button>
+										</div>
+									</form>
+								</Modal>
+							</>
 						)}
 					</div>
 				</div>
@@ -158,19 +248,19 @@ export default function ProfileCard({ profile }: Props) {
 					)}
 
 					{isMyProfile && (
-						<button
+						<Button
 							className="absolute left-10 bottom-[-70px] px-4 py-2 bg-red-400 text-white rounded hover:bg-red-600"
-							onClick={() => setIsModalOpen(true)}
+							onClick={() => setIsDeleteModalOpen(true)}
 						>
 							<Trash2 />
-						</button>
+						</Button>
 					)}
 				</div>
 			</div>
 			{/* 모달 컴포넌트 */}
 			<Modal
-				isOpen={isModalOpen}
-				onClose={() => setIsModalOpen(false)}
+				isOpen={isDeleteModalOpen}
+				onClose={() => setIsDeleteModalOpen(false)}
 				title="회원 탈퇴"
 			>
 				<p className="text-gray-600 mb-5">
@@ -182,22 +272,22 @@ export default function ProfileCard({ profile }: Props) {
 						""
 					) : (
 						<>
-							<button
-								onClick={() => setIsModalOpen(false)}
+							<Button
+								onClick={() => setIsDeleteModalOpen(false)}
 								className="px-8 py-3 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300 transition-colors"
 							>
 								취소
-							</button>
+							</Button>
 						</>
 					)}
 
-					<button
+					<Button
 						onClick={handleDeleteAccount}
 						disabled={isDeleting}
 						className="px-8 py-3 rounded-md bg-red-500 text-white hover:bg-red-600 transition-colors"
 					>
 						{isDeleting ? "삭제 중..." : "삭제"}
-					</button>
+					</Button>
 				</div>
 			</Modal>
 		</>
